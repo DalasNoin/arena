4c57287cb0a371c3a520a64a8d9dde1a7b13333a
DalasNoin <sim.ler68@gmail.com>
2022-10-27 14:56:50 +0800

    test of transformer on toy example

--- w1/modules.py
+++ w1/modules.py
@@ -11,7 +11,7 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         self.max_len = max_len
         L = self.max_len
-        partial_term = torch.outer(torch.arange(L),1/10_000**(torch.arange(torch.ceil(self.d_model/2))*2/d))
+        partial_term = torch.outer(torch.arange(L),1/10_000**(torch.arange(torch.ceil(torch.tensor(self.d_model/2)))*2/self.d_model))
         positional_encoding = torch.zeros((L, self.d_model))
         positional_encoding[:,::2] = torch.sin(partial_term)
         positional_encoding[:,1::2] = torch.cos(partial_term)
